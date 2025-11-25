eb185c028f419be408f4c6bb38ad7fc459353b56
buniary <118801603+buniary@users.noreply.github.com>
2025-11-25 16:44:31 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,79 @@
 // back to top
+const bttBtn = document.querySelector('#btt');
+
+//윈도우에 스크롤이 생기면 300이상이면 버튼 나타나고, 이하면 사라진다. 버튼을 클릭하면 화면 상단으로 부드럽게 이동한다.
+
+//윈도우에 스크롤이 생기면
+window.addEventListener('scroll', () => {
+  //scr에 윈도우 scrollY 값을 저장
+  let scr = window.scrollY;
+  //scrollY의 값이 300 이하면
+  if (scr >= 300) {
+    //버튼 노출
+    bttBtn.classList.add('active');
+  } else {
+    //버튼 제거
+    bttBtn.classList.remove('active');
+  }
+});
+
+//btt 버튼 클릭하면
+bttBtn.addEventListener('click', (e) => {
+  //기본 a 링크 제거
+  e.preventDefault();
+  //window 0,0 값으로 이동
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: 'smooth'
+  });
+});
 
 
 //horizontal 활용
+const slideWrapper = document.querySelector('.testimonial');
+const slideContainer = slideWrapper.querySelector('ul');
+const slides = slideContainer.querySelectorAll('li');
+
+const prevBtn = slideWrapper.querySelector('.prev');
+const nextBtn = slideWrapper.querySelector('.next');
+
+
+
+let currentIdx = 0;
+function moveSlide(idx) {
+  //전체 컨테이너에서 slides의 넓이 100% 만큼 이동
+  slideContainer.style.left = -idx * 100 + '%';
+  currentIdx = idx;
+}
+
+nextBtn.addEventListener('click', () => {
+  let nextIdx = (currentIdx + 1) % slides.length;
+  moveSlide(nextIdx);
+});
+
+prevBtn.addEventListener('click', () => {
+  let prevIdx = (currentIdx - 1 + slides.length) % slides.length;
+  moveSlide(prevIdx);
+});
+
+
 
 // accordion
+const quest = document.querySelectorAll('.question');
+const answer = document.querySelectorAll('.answer');
+
+//question 클릭하면 question 클래스에 active 추가
+
+quest.forEach((item) => {
+  item.addEventListener('click', () => {
+    for (let q of quest) {
+      q.classList.remove('active');
+    }
+    item.classList.add('active');
+
+  });
+})
+
+//question에 active 된 곳에 answer도 active
+
